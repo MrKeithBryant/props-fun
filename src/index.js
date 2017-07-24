df61b40e89553451498be7bdb342a79000bd1880
Keith Bryant <kabryant@cinci.rr.com>
2017-07-24 16:47:04 -0400

    Whoops

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-import App from './components/App.js'
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
