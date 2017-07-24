dff5e90384594227f89a51c25cf2ae6fb711ed6f
Keith Bryant <kabryant@cinci.rr.com>
2017-07-24 16:43:18 -0400

    props-fun

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import App from './components/App.js'
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
